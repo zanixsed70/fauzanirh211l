b1531b4a22003039150d71fa55da3b37dfd406fd
FauzaniRh <fauzanirahman737@gmail.com>
2021-11-20 07:43:04 +0800

    Initial commit

---
+++ app/src/main/java/com/fauzanirh/cobafauzan/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.fauzanirh.cobafauzan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val phoneEditText = findViewById<EditText>(R.id.phoneEditText)
+        val tanggalLahirEditText = findViewById<EditText>(R.id.tanggalLahirEditText)
+        val jumlahSksEditText = findViewById<EditText>(R.id.jumlahSksEditText)
+
+        simpanButton.setOnClickListener{
+            val nama = namaEditText.text.toString()
+            var jumlahSks = jumlahSksEditText.text.toString().toInt()
+
+            jumlahSks = 160 - jumlahSks
+            Toast.makeText(this,"Sisa SKS...$jumlahSks", Toast.LENGTH_SHORT).show()
+        }
+
+        batalButton.setOnClickListener{
+            Toast.makeText(this, "Batal...", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
